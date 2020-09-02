f5c5d7d485779210908a3af6bf1c1f4f751863cb
Luis Vega <vegaluisjose@gmail.com>
2020-09-02 08:49:47 -0700

    add print func

--- test/test.cc
+++ test/test.cc
@@ -2,6 +2,15 @@
 #include <vta/hw_spec.h>
 #include <stdio.h>
 
+void print_instr(VTAGemInsn* instr) {
+    uint8_t total = sizeof(VTAGenericInsn);
+    uint8_t* bytes = reinterpret_cast<uint8_t*>(instr);
+    for (int i = 0; i < total; i++) {
+        printf("%02x", bytes[total-i-1]);
+    }
+    printf("\n");
+}
+
 int main() {
     uint32_t num_instr = 1;
     uint32_t wait_cycles = 1000;
@@ -12,7 +21,12 @@ int main() {
     buf = VTAMemAlloc(sizeof(VTAGemInsn)*num_instr, 0);
 
     VTAGemInsn* instr = reinterpret_cast<VTAGemInsn*>(buf);
+
     instr[0].opcode = VTA_OPCODE_FINISH;
+    instr[1].opcode = VTA_OPCODE_GEMM;
+
+    print_instr(&instr[0]);
+    print_instr(&instr[1]);
 
     VTADeviceRun(handle, VTAMemGetPhyAddr(buf), num_instr, wait_cycles);
 
