091721a8a50cf0ae5f39f8f2a8037599ba3a75ee
Luis Vega <vegaluisjose@gmail.com>
2020-09-02 08:34:56 -0700

    add small example with finish instr

--- test/test.cc
+++ test/test.cc
@@ -1,7 +1,23 @@
 #include <vta/driver.h>
+#include <vta/hw_spec.h>
+#include <stdio.h>
 
 int main() {
+    uint32_t num_instr = 1;
+    uint32_t wait_cycles = 1000;
     void* buf;
-    buf = VTAMemAlloc(4, 1);
+    VTADeviceHandle handle;
+
+    handle = VTADeviceAlloc();
+    buf = VTAMemAlloc(sizeof(VTAGemInsn)*num_instr, 0);
+
+    VTAGemInsn* instr = reinterpret_cast<VTAGemInsn*>(buf);
+    instr[0].opcode = VTA_OPCODE_FINISH;
+
+    VTADeviceRun(handle, VTAMemGetPhyAddr(buf), num_instr, wait_cycles);
+
+    VTAMemFree(buf);
+    VTADeviceFree(handle);
+
     return 0;
 }
